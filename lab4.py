c1cf94c0eed6c6dab72149c1243fdb2da1acbd4f
NateR2 <7natereed7@gmail.com>
2023-09-26 15:00:43 -0400

    9/26

--- lab4.py
+++ lab4.py
@@ -1,6 +1,14 @@
 import openpyxl as op
-wb= op.load_workbook("Lab4Data.xlsx", read_only=True, data_only=True)
+import csv
+wb = op.load_workbook("Lab4Data.xlsx", read_only=True, data_only=True)
 
-ws=wb["Table 9"]
+ws = wb.active
 
-for row in ws[]
+categoryNames = ["Child Labour Total", "Child Labour Male", "Child Labour Female", "Child marriage <15", "Child marriage <18", "Birth Registration Total", "FGM Prevalence Women", "FGM Prevalence Girls", "FGM Support", "Wife Beating Justification Male", "Wife Beating Justification Female", "Violent Discipline Total", "Violent Discipline Male", "Violent Discipline Female"]
+
+outputList = []
+
+for row in ws["B15:AE211"]:
+    outputList.append(row[1].value+categoryNames[1] + str(row[3].value))
+
+print(outputList)
